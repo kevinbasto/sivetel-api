559194f6d2a7577f8b5b45bee77ee0ad09267498
Daniel R Basto <tronxx@gmail.com>
2025-10-13 12:44:24 -0600

    feat: add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReposModule } from './repos/repos.module';
 import { EndpointsModule } from './endpoints/endpoints.module';
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
     ReposModule, 
     EndpointsModule,
-    ConfigModule.forRoot({}),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [
+        ConfigModule
+      ],
+      inject: [
+        ConfigService
+      ],
+      useFactory: (ConfigService: ConfigService) => ({
+        type: ConfigService.get<string>('DATA_TYPE') as 'mysql', 
+        host: ConfigService.get<string>('DATA_HOST'),
+        port: ConfigService.get<number>('DATA_PORT'),
+        database: ConfigService.get<string>('DATA_DB'), 
+        username: ConfigService.get<string>('DATA_USER'),
+        // password: ConfigService.get<string>('DATA_PASSWORD'),
+      })
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
