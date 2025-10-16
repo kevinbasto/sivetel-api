ea17a1dc4179e590e20a53d56ad6f3e427b7de2c
kevin <bastokevindaniel@gmail.com>
2025-10-15 20:55:44 -0600

    [FEAT]: balances implemented

--- src/endpoints/endpoints.module.ts
+++ src/endpoints/endpoints.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { BalancesModule } from './balances/balances.module';
 
 
 @Module({
-  imports: [UsersModule, AuthModule, ProductsModule]
+  imports: [UsersModule, AuthModule, ProductsModule, BalancesModule]
 })
 export class EndpointsModule {}
